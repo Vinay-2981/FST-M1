8dba7541ce24c699933461003af67066b02056a6
Vinay-2981 <vinay.todupunuri@ibm.com>
2026-01-23 20:08:53 +0530

    Add files via upload

---
+++ TestNG/Activities/Activity2.java
@@ -0,0 +1,58 @@
+package TestNg;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Activity2 {
+ WebDriver driver;
+ 
+ @BeforeClass
+ 
+ public void beforeMethod() {
+	 driver = new FirefoxDriver();
+	 driver.get("https://training-support.net/webelements/target-practice");
+ }
+ 
+ @Test
+ 
+ public void testCase1() {
+	 
+	 String title = driver.getTitle();
+	 System.out.println("Title is : " + title);
+	 Assert.assertEquals(title, "Selenium: Target Practice");
+ }
+ 
+ @Test
+ 
+ 
+ public void testcase2() {
+	 WebElement blackbutton = driver.findElement(By.cssSelector("button.black"));
+	 Assert.assertTrue(blackbutton.isDisplayed());
+	 Assert.assertEquals(blackbutton.getText(), "Black");
+ }
+ 
+ @Test(enabled = false)
+ 
+ public void testCase3() {
+	 String subHeading = driver.findElement(By.className("sub")).getText();
+	 Assert.assertTrue(subHeading.contains("Practice"));
+ }
+ 
+ @Test
+ 
+ public void testCase4() {
+	 throw new SkipException("Skipping test case");
+ }
+ 
+ @AfterClass
+ public void afterMethod() {
+	 driver.close();
+ }
+}
